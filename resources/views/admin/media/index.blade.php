44a431b0b5a73469c94b97232975e79b383007a8
Fred Chute <fchute@schoolclimate.org>
2017-09-07 10:07:55 -0400

    media section complete

---
+++ resources/views/admin/media/index.blade.php
@@ -0,0 +1,60 @@
+@extends ('layouts.admin')
+
+
+
+@section('content')
+
+  @if(Session::has('deleted_media'))
+
+    <p class='bg-danger'> {{session('deleted_media')}}</p>
+
+  @endif
+
+  <h1>Media</h1>
+
+@if($photos)
+
+
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Created</th>
+
+        </tr>
+      </thead>
+      <tbody>
+
+    @foreach ($photos as $photo)
+
+        <tr>
+          <td>{{$photo->id}}</td>
+          <td><img height="50" src="{{$photo->file}}" alt=""></td>
+          <td>{{$photo->created_at ? $photo->created_at : 'no date'}}</td>
+
+
+          <td>
+
+             {!! Form::open(['method'=>'DELETE', 'action'=> ['AdminMediasController@destroy', $photo->id]]) !!}
+
+            <div class="form-group">
+                  {!! Form::label('title', 'Title:') !!}
+                  {!! Form::text('title', null, ['class'=>'form-control'])!!}
+            </div>
+
+              <div class="form-group">
+                {!! Form::submit('Delete', ['class'=>'btn btn-danger']) !!}
+                </div>
+
+              {!! Form::close() !!}
+
+            </td>
+        </tr>
+
+      @endforeach
+      </tbody>
+    </table>
+
+@endif
+@stop
